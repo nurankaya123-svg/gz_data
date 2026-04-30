d6c759b6847745f4c3261874b1256c9aa09f2ae7
Nuran Kaya <nurankaya123@gmail.com>
2026-04-30 08:53:28 +0000

    stg dosyaları temizlendi

---
+++ models/staging/raw/stg_raw__bing.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'bing') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name as campaign_name,
+        cast(ads_cost as float64) as ads_cost,
+        impression as ads_impression,
+        click as ads_clicks
+
+    from source
+
+)
+
+select * from renamed
